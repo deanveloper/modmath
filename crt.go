cedb5a517cfd6f61b1780bbc68c70a204b036d37
deanveloper <dean@deanveloper.com>
2018-03-03 02:24:05 -0500

    Added chinese remainder theorem stuff

---
+++ crt.go
@@ -0,0 +1,40 @@
+package modmath
+
+// This file is for stuff related to the chinese remainder theorem!
+
+// Solves x=a mod n; x=b mod m by using the chinese remainder theorem.
+func SolveCrt(a, n, b, m int) int {
+	s, t := eea(n, m)
+	return b * s * m + a * n * t
+}
+
+// Represents an entry in the Extended Chinese Remainder Theorem
+type CrtEntry struct {
+	A, N int
+}
+
+// Solves the solution to x=(a1 mod m1); x=(a2 mod m2); x=...
+//
+// If len(eqs) == 0, it panics.
+func SolveCrtMany(eqs []CrtEntry) int {
+	if len(eqs) == 0 {
+		return 0
+	}
+	if len(eqs) == 1 {
+		return Lpr(eqs[0].A, eqs[0].N)
+	}
+	eqs2 := make([]CrtEntry, len(eqs))
+	copy(eqs2, eqs)
+	return solveCrtManyIntern(eqs2)
+}
+
+func solveCrtManyIntern(eqs []CrtEntry) int {
+	f := eqs[0]
+	s := eqs[1]
+	x := SolveCrt(f.A, f.N, s.A, s.N)
+	if len(eqs) == 2 {
+		return x
+	}
+	eqs[1] = CrtEntry{x, f.N * s.N}
+	return solveCrtManyIntern(eqs[1:])
+}
